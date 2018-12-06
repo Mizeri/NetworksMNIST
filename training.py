467fe7d6c0983fadc7dc5e4bdb1aefb01a5447c5
赵钎 <zhaoqian@zhaoqiandeMacBook-Pro-2.local>
2018-12-06 11:35:13 +1100

    Update training.py

--- training.py
+++ training.py
@@ -2,6 +2,15 @@
 import mnist_loader
 import network
 
+# Defining 'training_data', 'validation_data', 'test_data', see mnist_loader.py
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-net = network.Network([784, 30, 10])
+
+'''Create a network with 3 layers: an input layer(28 * 28 = 784 input neurons),
+a hidden layer(30 neurons), an output layer(10 neurons representing 10 digits)
+'''
+net = network.Network([784, 32, 10])
+
+'''Stochastic gradient descent algorithm, 30 epochs, mini_batch_size = 10,
+eta = 3.0
+'''
 net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
